e9f313884be06c04e252b3d06438caa4864f4903
Dickson Tam <dicksont650@gmail.com>
2015-05-25 19:27:18 -0700

    Fixed closure. Added enumeration.

--- index.js
+++ index.js
@@ -25,271 +25,313 @@
  */
 
 (function(property) {
-
   var property = property || 'bind';
+  var rclass = /^[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*$/;
+  var rattrib = /^[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*$/;
 
-  var attachBindToElementPrototype = function(element, property) {
+  function truthy(str) {
+    if (str == null) return false;
 
-    var opts = [];
-    var rclass = /^[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*$/;
-    var rattrib = /^[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*$/;
+    var str = str.trim().toLowerCase();
+    return str != 'no' && str != 'false' && str != '0';
+  }
 
-    Object.defineProperty(element.prototype, property, { get: getBind });
+  function checkClassName(className) {
 
-    function addOptions() {
-      var args = Array.prototype.slice.call(arguments);
-      opts = opts.concat(args);
-
-      return { to: createBinding.bind(this) };
-    }
-
-    function truthy(str) {
-      if (str == null) return false;
-
-      var str = str.trim().toLowerCase();
-      return !!str && str != 'no' && str != 'false' && str != '0';
-    }
-
-    function createBinding(obj, prop) {
-      var el = this;
-      var getter, setter;
-
-      if (!(el instanceof HTMLElement)) {
-        throw new Error('Element ' + el + ' is not a HTMLElement');
-      }
-
-      if (obj.hasOwnProperty(prop)) {
-        throw new Error('Object ' + obj + ' already has property' + prop);
-      }
-
-      switch (opts[0]) {
-        case 'text':
-          getter = function() {
-            return el.innerText;
-          };
-          setter = function(text) {
-            el.innerText = text;
-          };
-          break;
-        case 'html':
-          getter = function() {
-            return el.innerHTML;
-          };
-          setter = function(html) {
-            el.innerHTML = html;
-          }
-          break;
-        case 'classList':
-          getter = function() {
-            return el.classList;
-          }
-          setter = function(arr) {
-            el.classList = arr;
-          }
-          break;
-        case 'hasClass':
-          getter = function() {
-            return el.classList.contains(opts[1]);
-          };
-          setter = function(boolean) {
-            if (boolean) {
-              el.classList.add(opts[1]);
-            } else {
-              el.classList.remove(opts[1]);
-            }
-          }
-          break;
-        case 'noClass':
-          getter = function() {
-            return !el.classList.contains(opts[1]);
-          };
-          setter = function(boolean) {
-            if (!boolean) {
-              el.classList.add(opts[1]);
-            } else {
-              el.classList.remove(opts[1]);
-            }
-          }
-          break;
-        case 'attr':
-          getter = function() {
-            return el.getAttribute(opts[1]);
-          };
-          setter = function(attr) {
-            el.setAttribute(opts[1], attr);
-          };
-          break;
-        case 'hasTruthyAttr':
-          getter = function() {
-            return truthy(el.getAtribute(opts[1]));
-          };
-          setter = function(boolean) {
-            var val = boolean? 'true' : 'false';
-            el.setAttribute(opts[1], val);
-          };
-          break;
-        case 'noTruthyAttr':
-          getter = function() {
-            return !truthy(el.getAtribute(opts[1]));
-          };
-          setter = function(boolean) {
-            var val = !boolean? 'true' : 'false';
-            el.setAttribute(opts[1], val);
-          };
-          break;
-        case 'textValue':
-          getter = function() {
-            return el.value;
-          };
-          setter = function(text) {
-            el.value = text;
-          };
-          break;
-        case 'selectValue':
-          getter = function() {
-            return el.value;
-          };
-          setter = function(text) {
-            el.value = text;
-          };
-          break;
-        case 'groupValue':
-          getter = function() {
-            var name = el.name;
-            var group = document.getElementsByName(name);
-
-            for (var i=0; i < group.length; i++) {
-              var elradio = group[i];
-
-              if (!(elradio instanceof HTMLInputElement)) continue;
-              if (!(elradio.type == 'radio')) continue;
-
-              if (elradio.checked) {
-                return elradio.value;
-              }
-            }
-
-            return undefined;
-          }
-          setter = function(value) {
-            var name = el.name;
-            var group = document.getElementsByName(name);
-
-            for (var i=0; i < group.length; i++) {
-              var elradio = group[i];
-
-              if (!(elradio instanceof HTMLInputElement)) continue;
-              if (!(elradio.type == 'radio')) continue;
-
-              elradio.checked = (elradio.value == value);
-            }
-          }
-          break;
-        case 'checked':
-          getter = function() {
-            return el.checked;
-          }
-          setter = function(boolean) {
-            el.checked = boolean;
-          }
-          break;
-      }
-
-      Object.defineProperty(obj, prop, { get: getter, set: setter, enumerable: true});
-
-    }
-
-    function checkClassName(className) {
+    if (className == null) {
       var fname = arguments.callee.caller.name;
-
-      if (className == null) {
-        throw new Error('className parameter required in ' + fname + ' call.');
-      } else if (typeof className != "string") {
-        throw new Error('className parameter is not a string.');
-      } else if (className.length == 0) {
-        throw new Error('className parameter is an empty string.');
-      } else if (className.match(rclass)){
-        throw new Error('className parameter is not valid class name.');
-      }
+      throw new Error('className parameter required in ' + fname + ' call.');
+    } else if (typeof className != "string") {
+      throw new Error('className parameter is not a string.');
+    } else if (className.length == 0) {
+      throw new Error('className parameter is an empty string.');
+    } else if (!className.match(rclass)){
+      throw new Error('className parameter is not valid class name.');
     }
+  }
 
-    function checkAttributeName(attribute) {
-      var fname = arguments.callee.caller.name;
+  function checkAttributeName(attribute) {
+    var fname = arguments.callee.caller.name;
 
-      if (attribute == null) {
-        throw new Error('Attribute name required in ' + fname + ' call.');
-      } else if (typeof attribute != "string") {
-        throw new Error('Attribute parameter is not a string.');
-      } else if (attribute.length == 0) {
-        throw new Error('Attribute parameter is an empty string.');
-      } else if (attribute.match(rattrib)){
-        throw new Error('Attribute parameter is not valid attribute name.');
-      }
+    if (attribute == null) {
+      throw new Error('Attribute name required in ' + fname + ' call.');
+    } else if (typeof attribute != "string") {
+      throw new Error('Attribute parameter is not a string.');
+    } else if (attribute.length == 0) {
+      throw new Error('Attribute parameter is an empty string.');
+    } else if (attribute.match(rattrib)){
+      throw new Error('Attribute parameter is not valid attribute name.');
     }
+  }
+
+  function attachBindToElementPrototype(element, property) {
+
+    Object.defineProperty(element.prototype, property, { get: getBind, enumerable: true});
 
     function getBind() {
+
+      var opts = [];
+
+      function addOptions() {
+        var args = Array.prototype.slice.call(arguments);
+        opts = opts.concat(args);
+
+        return { to: createBinding.bind(this) };
+      }
+
+      function createBinding(obj, prop) {
+        var el = this;
+        var getter, setter;
+
+        if (!(el instanceof HTMLElement)) {
+          throw new Error('Element ' + el + ' is not a HTMLElement');
+        }
+
+/*
+        if (obj.hasOwnProperty(prop)) {
+          throw new Error('Object ' + obj + ' already has property ' + prop);
+        }
+*/
+
+        switch (opts[0]) {
+          case 'text':
+            getter = function() {
+              return el.textContent || el.innerText;
+            };
+            setter = function(text) {
+              el.innerText = text;
+            };
+            break;
+          case 'html':
+            getter = function() {
+              return el.innerHTML;
+            };
+            setter = function(html) {
+              el.innerHTML = html;
+            }
+            break;
+          case 'classList':
+            getter = function() {
+              return Array.prototype.slice.call(el.classList);
+            }
+            setter = function(arr) {
+              var last = el.classList.length - 1;
+              for (var i=last; i >= 0; i--) {
+                el.classList.remove(el.classList[i]);
+              }
+
+              for (var i=0; i < arr.length; i++) {
+                el.classList.add(arr[i]);
+              }
+            }
+            break;
+          case 'hasClass':
+            getter = function() {
+              return el.classList.contains(opts[1]);
+            };
+            setter = function(boolean) {
+              if (boolean) {
+                el.classList.add(opts[1]);
+              } else {
+                el.classList.remove(opts[1]);
+              }
+
+            }
+            break;
+          case 'noClass':
+            getter = function() {
+              return !el.classList.contains(opts[1]);
+            };
+            setter = function(boolean) {
+              if (!boolean) {
+                el.classList.add(opts[1]);
+              } else {
+                el.classList.remove(opts[1]);
+              }
+            }
+            break;
+          case 'attr':
+            getter = function() {
+              return el.getAttribute(opts[1]);
+            };
+            setter = function(val) {
+              el.setAttribute(opts[1], Object(val).toString());
+            };
+            break;
+          case 'hasTruthyAttr':
+            getter = function() {
+              return truthy(el.getAttribute(opts[1]));
+            };
+            setter = function(val) {
+              if (val == null) {
+                el.removeAttribute(opts[1]);
+              } else {
+                el.setAttribute(opts[1], Object(val).toString());
+              }
+            };
+            break;
+          case 'noTruthyAttr':
+            getter = function() {
+              return !truthy(el.getAttribute(opts[1]));
+            };
+            setter = function(boolean) {
+              var val = !boolean? 'true' : 'false';
+              el.setAttribute(opts[1], val);
+            };
+            break;
+          case 'textValue':
+            getter = function() {
+              return el.value;
+            };
+            setter = function(text) {
+              el.value = text;
+            };
+            break;
+          case 'selectValue':
+            getter = function() {
+              return el.value;
+            };
+            setter = function(text) {
+              
+              el.value = text;
+            };
+            break;
+          case 'groupValue':
+            getter = function() {
+              var name = el.name;
+              var group = document.getElementsByName(name);
+
+              for (var i=0; i < group.length; i++) {
+                var elradio = group[i];
+
+                if (!(elradio instanceof HTMLInputElement)) continue;
+                if (!(elradio.type == 'radio')) continue;
+
+                if (elradio.checked) {
+                  return elradio.value;
+                }
+              }
+
+              return null;
+            }
+            setter = function(value) {
+              var name = el.name;
+              var group = document.getElementsByName(name);
+              var found = false;
+
+              for (var i=0; i < group.length; i++) {
+                var elradio = group[i];
+
+                if (!(elradio instanceof HTMLInputElement)) continue;
+                if (!(elradio.type == 'radio')) continue;
+
+                if (elradio.value == value) {
+                  elradio.checked = true;
+                  found = true;
+                } else {
+                  elradio.checked = false;
+                }
+              }
+
+              if (value != null && !found)
+                throw new Error('Failed to set value ' + ((typeof(value) == 'string')? "'" + value + "'" : value) + ' for radio group ' + name);
+            }
+            break;
+          case 'checked':
+            getter = function() {
+              return el.checked;
+            }
+            setter = function(boolean) {
+              el.checked = boolean;
+            }
+            break;
+          default:
+            getter = function() {
+              throw new Error('Option ' + opts[0] + ' does not exist');
+            }
+            setter = function() {
+              throw new Error('Option ' + opts[0] + ' does not exist');
+            }
+        }
+
+        Object.defineProperty(obj, prop, {
+          get: getter,
+          set: setter,
+          enumerable: true,
+          configurable: true
+        });
+      }
+
       var el = this;
       var bind = {
-        classList: function(className) {
-          checkClassName(className);
-          addOptions.bind(el, 'classList');
-        },
         hasClass: function(className) {
           checkClassName(className);
-          addOptions.bind(el, 'hasClass');
+          return (addOptions.bind(el, 'hasClass'))(className);
         },
         noClass: function(className) {
           checkClassName(className);
-          addOptions.bind(el, 'noClass');
+          return (addOptions.bind(el, 'noClass'))(className);
         },
         attr: function(attr) {
           checkAttributeName(attr);
-          addOptions.bind(el, 'attr');
+          return (addOptions.bind(el, 'attr'))(attr);
         },
         hasTruthyAttr: function(attr) {
           checkAttributeName(attr);
-          addOptions.bind(el, 'hasTruthyAttr');
+          return (addOptions.bind(el, 'hasTruthyAttr'))(attr);
         },
         noTruthyAttr: function(attr) {
           checkAttributeName(attr);
-          addOptions.bind(el, 'noTruthyAttr');
+          return (addOptions.bind(el, 'noTruthyAttr'))(attr);
         }
       };
 
+      Object.defineProperty(bind, 'classList', {
+        get: addOptions.bind(el, 'classList'),
+        enumerable: true
+      });
+
       Object.defineProperty(bind, 'text', {
-        get: addOptions.bind(el, 'text')
+        get: addOptions.bind(el, 'text'),
+        enumerable: true
       })
 
       Object.defineProperty(bind, 'html', {
-        get: addOptions.bind(el, 'html')
+        get: addOptions.bind(el, 'html'),
+        enumerable: true
       })
 
       if (element == HTMLSelectElement) {
         Object.defineProperty(bind, 'selectValue', {
-          get: addOptions.bind(el, 'selectValue')
+          get: addOptions.bind(el, 'selectValue'),
+          enumerable: true
         })
       } else if (element == HTMLInputElement) {
-        switch (element.type) {
+        switch (el.type) {
           case 'password':
           case 'text':
             Object.defineProperty(bind, 'textValue', {
-              get: addOptions.bind(el, 'textValue')
+              get: addOptions.bind(el, 'textValue'),
+              enumerable: true
             });
             break;
           case 'radio':
             Object.defineProperty(bind, 'groupValue', {
-              get: addOptions.bind(el, 'groupValue')
+              get: addOptions.bind(el, 'groupValue'),
+              enumerable: true
             });
             break;
           case 'checkbox':
             Object.defineProperty(bind, 'checked', {
-              get:  addOptions.bind(el, 'checked')
+              get:  addOptions.bind(el, 'checked'),
+              enumerable: true
             });
             break;
         }
       } else if (element == HTMLTextAreaElement) {
         Object.defineProperty(bind, 'textValue', {
-          get: addOptions.bind(el, 'textValue')
+          get: addOptions.bind(el, 'textValue'),
+          enumerable: true
         });
       }
 
